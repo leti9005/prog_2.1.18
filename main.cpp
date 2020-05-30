52252bfcaa7a50cfa7a79bd37017fba8017448e7
fiCeVitka <gmac.net.ru@gmail.com>
2020-05-30 20:05:16 +0300

    Подключил файлы

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include <string.h>
+#include <fstream>
 
-int main () {
-	std::cout << "test" << std::endl;
+using namespace std;
+
+//todo: Относительный путь почему-то не работает
+const string input = "/Users/Victor/CLionProjects/prog_2.1.12/input.txt";
+const string output = "output.txt";
+
+int main() {
+	string text;
+
+	ifstream inputFile(input, ios::in);
+
+	if (inputFile.is_open()) {
+		while (getline(inputFile, text)) {
+			cout << text << endl;
+		}
+	}
+
+	inputFile.close();
 }
